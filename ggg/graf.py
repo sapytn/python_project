253f789f93fa55980d7d59ca6b8397b50c1c1ec9
sapytn <sapytn@gmail.com>
2021-11-21 19:22:41 +0300

    create graphic

--- ggg/graf.py
+++ ggg/graf.py
@@ -1,49 +1,44 @@
 # импортируем модули
-import numpy as np
-import matplotlib.pyplot as plt
 import pylab
 import matplotlib.path
-
-# функция
-# y = lambda x: np.sin(x)
-# создаём рисунок с координатную плоскость
-fig = plt.subplots()
-# создаём область, в которой будет
-# - отображаться график
-# x = np.linspace(-3, 3,100)
-# значения x, которые будут отображены
-# количество элементов в созданном массиве
-# - качество прорисовки графика
-# рисуем график
-x = [10, 37.21405430584632, 82.78594569415368, 110]
-y = [10, 70.2859456941537, 70.2859456941537, 10]
-#
-# plt.plot(x,y)
-
-plt.plot(x, y)
+from microprogram import calc_points
 
 
-def drawArc(axes):
+def draw_arc(axes, arc_x, arc_y, arc_width, arc_theta1, arc_theta2):
     """
     Рисование дуги
     """
-    arc_x = 0
-    arc_y = 0
-    arc_width = 1
-    arc_height = 1
-    arc_theta1 = 270
-    arc_theta2 = 360
+    # arc_x = 25
+    # arc_y = 35
+    arc_height = arc_width
+    # arc_theta1 = 0
+    # arc_theta2 = 180
 
     arc = matplotlib.patches.Arc((arc_x, arc_y),
                                  arc_width,
                                  arc_height,
                                  theta1=arc_theta1,
-                                 theta2=arc_theta2)
+                                 theta2=arc_theta2
+                                 )
     axes.add_patch(arc)
-    pylab.text(0.6, -0.3, "Arc", horizontalalignment="center")
 
-drawArc(50,50,25,25)
 
+param = (0, 0, 50, 50, 25, 25, 25)
+param1 = (50, 50, 100, 0, 25, -25, 25)
+param2 = (100, 0, 50, -50, -25, -25, 25)
+param3 = (-50, -50, 0, 0, -25, 25, 25)
+
+
+axes = pylab.gca()
+axes.set_aspect("equal")
+
+# x, y, i, j, r, arc_theta1, arc_theta2 = calc_points(param)
+# draw_arc(axes, i, j, r * 2, arc_theta2, arc_theta1)
+
+x, y, i, j, r, arc_theta1, arc_theta2 = calc_points(param)
+draw_arc(axes, i, j, r * 2, arc_theta2, arc_theta1)
+
+pylab.plot(x, y)
 
 # показываем график
-plt.show()
+pylab.show()
--- ggg/microprogram.py
+++ ggg/microprogram.py
@@ -1,9 +1,7 @@
 import math
 
-param = (0, 0, 70, 10, 25, 35, 25)
 
-
-# param = (10, 0, 100, 0, 45, 50, 25)
+# param = (10, 10, 110, 10, 45, 50, 25)
 
 
 def calc_angle(x_radius, y_radius, r):
@@ -20,14 +18,16 @@ def calc_angle(x_radius, y_radius, r):
 
 def calc_points(param):
     x_start, y_start, x_end, y_end, i, j, r = param
-    # print(f'x0 {x_start} y0 {y_start}')
-    print(x_start, y_start)
+    x = []
+    y = []
 
     x_radius = i
     y_radius = j
 
     x_point = x_start
     y_point = y_start
+    x.append(x_point)
+    y.append(y_point)
 
     if i == r:
         y_point += y_radius
@@ -49,41 +49,66 @@ def calc_points(param):
             x_point = x_start - abs(x_distance)
             y_point = y_start - abs(y_distance)
     # print(f'x1 {x_pont} y1 {y_point}')
-    print(x_point, y_point)
+    # print(x_point, y_point)
+    x.append(x_point)
+    y.append(y_point)
+    first_angle = math.degrees(math.acos((x_point - (x_start + x_radius)) / r))
+    # FIRST POINT IS FIND
+
     x_point, y_point = x_end, y_end
     x_radius = x_end - (x_start + i)
     y_radius = y_end - (y_start + j)
-    print(f'x{x_radius} y{y_radius}')
 
-    if x_radius == r:
+    if x_radius == r and y_radius == y_end:
         y_point -= y_radius
 
-    elif y_radius == r:
+    elif x_radius == r:
         x_point -= x_radius
+
+    elif y_radius == r and x_radius == x_end:
+        x_point -= x_radius
+
+    elif y_radius == r:
+        y_point -= y_radius
 
     else:
         distance_start, sin_angle, cos_angle = calc_angle(x_radius, y_radius, r)
-        if cos_angle == 0 or sin_angle == 0:
-            # print(f'x2 {x_end - distance_start * sin_angle} y2 {y_end + distance_start * cos_angle}')
-            print(x_end - distance_start * sin_angle, y_end + distance_start * cos_angle)
+        # if x_end - x_radius == r:
+        #     x_point = x_end - x_radius
+        # if y_end - y_radius == r:
+        #     y_point = y_end - y_radius
+
+        # print(f'x2 {x_end - distance_start * sin_angle} y2 {y_end + distance_start * cos_angle}')
+        # print(x_end - distance_start * sin_angle, y_end + distance_start * cos_angle)
+
+        x_point, y_point = distance_start * cos_angle, distance_start * sin_angle
+        if i > 0 and j > 0:
+            x_point = x_end - abs(x_point)
+            y_point = y_end + abs(y_point)
+        elif i < 0 and j > 0:
+            x_point += abs(x_point)
+            y_point += abs(y_point)
+        elif i > 0 and j < 0:
+            x_point -= abs(x_point)
+            y_point -= abs(y_point)
         else:
-            x_pont, y_point = distance_start * cos_angle, distance_start * sin_angle
-            if i > 0 and j > 0:
-                x_point -= abs(x_pont)
-                y_point += abs(y_point)
-            elif i < 0 and j > 0:
-                x_point += abs(x_pont)
-                y_point += abs(y_point)
-            elif i > 0 and j < 0:
-                x_point -= abs(x_pont)
-                y_point -= abs(y_point)
-            else:
-                x_point += abs(x_pont)
-                y_point -= abs(y_point)
+            x_point += abs(x_point)
+            y_point -= abs(y_point)
+
     # print(f'x2 {x_pont} y2 {y_point}')
-    print(x_point, y_point)
+    # print(x_point, y_point)
+    x.append(x_point)
+    y.append(y_point)
+    second_angle = math.degrees(math.acos((x_point - (x_end - x_radius)) / r))
     # print(f'x_end {x_end} y_end {y_end}')
-    print(x_end, y_end)
+    # print(x_end, y_end)
+    x.append(x_end)
+    y.append(y_end)
+    return x, y, i + x_start, j + y_start, r, first_angle, second_angle
 
 
-calc_points(param)
+if __name__ == '__main__':
+    param = (0, 0, 50, 50, 25, 25, 25)
+    x, y, i, j, r, arc_theta1, arc_theta2 = calc_points(param)
+    print("x", x, "y", y)
+    print(i, j, r)
